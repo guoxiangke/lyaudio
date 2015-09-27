bb2e03ddc70368a6da4264bccdbd477bfa69dcc1
dale <dale.guo@loweprofero.com>
2015-09-27 16:34:15 +0800

    debug

--- get_mp3_link_wx.php
+++ get_mp3_link_wx.php
@@ -13,7 +13,10 @@ if(file_exists($file_key))  {
     return;
 }
 //1点到2点之间执行！！！
-if(!(date('G')>1&&date('G')<2)) return;
+if(!(date('G')>1&&date('G')<2)) {
+    echo 'From '.$_SERVER['REMOTE_HOST'];
+    return;
+}
 // $file_store_key = $file_path .'/store/'. date('Ymd') . '.txt';
 // chmod($file_store_key, 0777); 
 chmod($file_key, 0777); 
