f11db505241b88e4b6f6f7bf3e0de39721cf16e9
dale <dale.guo@loweprofero.com>
2015-10-07 16:41:44 +0800

    txly1

--- cloud_get.php
+++ cloud_get.php
@@ -1,11 +1,13 @@
+<meta charset="UTF-8">
 <pre><?php
+date_default_timezone_set('Asia/Shanghai');
 //check if get already? cron once a day!
 $file_path = dirname(__FILE__).'/cron/cloud/';
 if (!is_dir($file_path)) {
     mkdir($file_path, 0777, true);
 }
 $file_key = $file_path . date('Ymd') . '.json';
-if(file_exists($file_key))  {
+if(file_exists($file_key)&&date('Ymd')!='20151007')  {
 	echo 'Warning: File ' . $file_key . ' exists! Exit!!!';
 	return;
 }
@@ -14,203 +16,203 @@ if(file_exists($file_key))  {
 $radios = array(
   array(
     'title' => '无限飞行号',
-    'url' => 'http://stream.liangyou.net/program/1njcjm-3b2p38',
+    'url' => 'http://www.txly1.net/program/1njcjm-3b2p38',
   ),
   array(
     'title' => 'i关怀心磁场',
-    'url' => 'http://stream.liangyou.net/program/1njchy-3b2ozw',
+    'url' => 'http://www.txly1.net/program/1njchy-3b2ozw',
   ),
   array(
     'title' => '空中辅导',
-    'url' => 'http://stream.liangyou.net/program/1njci2-3b2p04',
+    'url' => 'http://www.txly1.net/program/1njci2-3b2p04',
   ),
   array(
     'title' => '恋爱季节',
-    'url' => 'http://stream.liangyou.net/program/1njci0-3b2p00',
+    'url' => 'http://www.txly1.net/program/1njci0-3b2p00',
   ),
   array(
     'title' => '幸福满家园',
-    'url' => 'http://stream.liangyou.net/program/1njci6-3b2p0c',
+    'url' => 'http://www.txly1.net/program/1njci6-3b2p0c',
   ),
   array(
     'title' => '亲情不断电',
-    'url' => 'http://stream.liangyou.net/program/1njcil-3b2p16',
+    'url' => 'http://www.txly1.net/program/1njcil-3b2p16',
   ),
   array(
     'title' => '欢乐卡恰碰',
-    'url' => 'http://stream.liangyou.net/program/1njci4-3b2p08',
+    'url' => 'http://www.txly1.net/program/1njci4-3b2p08',
   ),
   array(
     'title' => '绝妙当家',
-    'url' => 'http://stream.liangyou.net/program/1njchx-3b2ozu',
+    'url' => 'http://www.txly1.net/program/1njchx-3b2ozu',
   ),
   array(
     'title' => '生活无国界',
-    'url' => 'http://stream.liangyou.net/program/1njci3-3b2p06',
+    'url' => 'http://www.txly1.net/program/1njci3-3b2p06',
   ),
   array(
     'title' => '零点零距离',
-    'url' => 'http://stream.liangyou.net/program/1njciw-3b2p1s',
+    'url' => 'http://www.txly1.net/program/1njciw-3b2p1s',
   ),
   array(
     'title' => '书香园地',
-    'url' => 'http://stream.liangyou.net/program/1njci1-3b2p02',
+    'url' => 'http://www.txly1.net/program/1njci1-3b2p02',
   ),
   array(
     'title' => 'i-Radio爱广播',
-    'url' => 'http://stream.liangyou.net/program/1njchu-3b2ozo',
+    'url' => 'http://www.txly1.net/program/1njchu-3b2ozo',
   ),
   array(
     'title' => 'Yi-Radio爱广播',
-    'url' => 'http://stream.liangyou.net/program/1njciv-3b2p1q',
+    'url' => 'http://www.txly1.net/program/1njciv-3b2p1q',
   ),
   array(
     'title' => '今夜心未眠',
-    'url' => 'http://stream.liangyou.net/program/1njcia-3b2p0k',
+    'url' => 'http://www.txly1.net/program/1njcia-3b2p0k',
   ),
   array(
     'title' => '听听90后',
-    'url' => 'http://stream.liangyou.net/program/1njci7-3b2p0e',
+    'url' => 'http://www.txly1.net/program/1njci7-3b2p0e',
   ),
   array(
     'title' => '彩虹桥',
-    'url' => 'http://stream.liangyou.net/program/1njchv-3b2ozq',
+    'url' => 'http://www.txly1.net/program/1njchv-3b2ozq',
   ),
   array(
     'title' => '现代人的希望',
-    'url' => 'http://stream.liangyou.net/program/1njcif-3b2p0u',
+    'url' => 'http://www.txly1.net/program/1njcif-3b2p0u',
   ),
   array(
     'title' => '生命的四季',
-    'url' => 'http://stream.liangyou.net/program/1njciu-3b2p1o',
+    'url' => 'http://www.txly1.net/program/1njciu-3b2p1o',
   ),
   array(
     'title' => '拥抱每一天',
-    'url' => 'http://stream.liangyou.net/program/1njchw-3b2ozs',
+    'url' => 'http://www.txly1.net/program/1njchw-3b2ozs',
   ),
   array(
     'title' => '灵命日粮',
-    'url' => 'http://stream.liangyou.net/program/1njcht-3b2ozm',
+    'url' => 'http://www.txly1.net/program/1njcht-3b2ozm',
   ),
   array(
     'title' => '真道分解',
-    'url' => 'http://stream.liangyou.net/program/1njcik-3b2p14',
+    'url' => 'http://www.txly1.net/program/1njcik-3b2p14',
   ),
   array(
     'title' => '圣言盛宴',
-    'url' => 'http://stream.liangyou.net/program/1njci8-3b2p0g',
+    'url' => 'http://www.txly1.net/program/1njci8-3b2p0g',
   ),
   array(
     'title' => '齐来颂扬',
-    'url' => 'http://stream.liangyou.net/program/1njchz-3b2ozy',
+    'url' => 'http://www.txly1.net/program/1njchz-3b2ozy',
   ),
   array(
     'title' => '长夜的牵引',
-    'url' => 'http://stream.liangyou.net/program/1njci9-3b2p0i',
+    'url' => 'http://www.txly1.net/program/1njci9-3b2p0i',
   ),
   array(
     'title' => '真理之光',
-    'url' => 'http://stream.liangyou.net/program/1njcjh-3b2p2y',
+    'url' => 'http://www.txly1.net/program/1njcjh-3b2p2y',
   ),
   array(
     'title' => '接棒人',
-    'url' => 'http://stream.liangyou.net/program/1njcj9-3b2p2i',
+    'url' => 'http://www.txly1.net/program/1njcj9-3b2p2i',
   ),
   array(
     'title' => '聆听主道',
-    'url' => 'http://stream.liangyou.net/program/1njciy-3b2p1w',
+    'url' => 'http://www.txly1.net/program/1njciy-3b2p1w',
   ),
   array(
     'title' => '空中崇拜',
-    'url' => 'http://stream.liangyou.net/program/1njcir-3b2p1i',
+    'url' => 'http://www.txly1.net/program/1njcir-3b2p1i',
   ),
   array(
     'title' => '善牧良言',
-    'url' => 'http://stream.liangyou.net/program/1njciz-3b2p1y',
+    'url' => 'http://www.txly1.net/program/1njciz-3b2p1y',
   ),
   array(
     'title' => '好牧人',
-    'url' => 'http://stream.liangyou.net/program/1njcix-3b2p1u',
+    'url' => 'http://www.txly1.net/program/1njcix-3b2p1u',
   ),
   array(
     'title' => '经动人心',
-    'url' => 'http://stream.liangyou.net/program/1njcis-3b2p1k',
+    'url' => 'http://www.txly1.net/program/1njcis-3b2p1k',
   ),
   array(
     'title' => '佳美脚踪',
-    'url' => 'http://stream.liangyou.net/program/1njcl2-3b2p64',
+    'url' => 'http://www.txly1.net/program/1njcl2-3b2p64',
   ),
   array(
     'title' => '蓝天绿洲',
-    'url' => 'http://stream.liangyou.net/program/1njcit-3b2p1m',
+    'url' => 'http://www.txly1.net/program/1njcit-3b2p1m',
   ),
   array(
     'title' => '给力人生',
-    'url' => 'http://stream.liangyou.net/program/1njcj7-3b2p2e',
+    'url' => 'http://www.txly1.net/program/1njcj7-3b2p2e',
   ),
   array(
     'title' => '空中门训',
-    'url' => 'http://stream.liangyou.net/program/1njcja-3b2p2k',
+    'url' => 'http://www.txly1.net/program/1njcja-3b2p2k',
   ),
   array(
     'title' => '生根建造',
-    'url' => 'http://stream.liangyou.net/program/1njcj5-3b2p2a',
+    'url' => 'http://www.txly1.net/program/1njcj5-3b2p2a',
   ),
   array(
     'title' => '信仰百宝箱',
-    'url' => 'http://stream.liangyou.net/program/1njcj8-3b2p2g',
+    'url' => 'http://www.txly1.net/program/1njcj8-3b2p2g',
   ),
   array(
     'title' => '生活之光',
-    'url' => 'http://stream.liangyou.net/program/1njcjc-3b2p2o',
+    'url' => 'http://www.txly1.net/program/1njcjc-3b2p2o',
   ),
   array(
     'title' => '生命的福音',
-    'url' => 'http://stream.liangyou.net/program/1njcjb-3b2p2m',
+    'url' => 'http://www.txly1.net/program/1njcjb-3b2p2m',
   ),
   array(
     'title' => '无愧的工人',
-    'url' => 'http://stream.liangyou.net/program/1njcj4-3b2p28',
+    'url' => 'http://www.txly1.net/program/1njcj4-3b2p28',
   ),
   array(
     'title' => '良友圣经学院（基础课程）',
-    'url' => 'http://stream.liangyou.net/program/1njck8-3b2p4g',
+    'url' => 'http://www.txly1.net/program/1njck8-3b2p4g',
   ),
   array(
     'title' => '良友圣经学院（本科文凭课程）（第一套）',
-    'url' => 'http://stream.liangyou.net/program/1njck9-3b2p4i',
+    'url' => 'http://www.txly1.net/program/1njck9-3b2p4i',
   ),
   array(
     'title' => '良友圣经学院（本科文凭课程）（第二套）',
-    'url' => 'http://stream.liangyou.net/program/1njcka-3b2p4k',
+    'url' => 'http://www.txly1.net/program/1njcka-3b2p4k',
   ),
   array(
     'title' => '良友圣经学院（进深文凭课程）（第一套）',
-    'url' => 'http://stream.liangyou.net/program/1njckb-3b2p4m',
+    'url' => 'http://www.txly1.net/program/1njckb-3b2p4m',
   ),
   array(
     'title' => '良友圣经学院（进深文凭课程）（第二套）',
-    'url' => 'http://stream.liangyou.net/program/1njckc-3b2p4o',
+    'url' => 'http://www.txly1.net/program/1njckc-3b2p4o',
   ),
   array(
     'title' => '晨曦讲座',
-    'url' => 'http://stream.liangyou.net/program/1njcji-3b2p30',
+    'url' => 'http://www.txly1.net/program/1njcji-3b2p30',
   ),
   array(
     'title' => '天路导向',
-    'url' => 'http://stream.liangyou.net/program/1njcj3-3b2p26',
+    'url' => 'http://www.txly1.net/program/1njcj3-3b2p26',
   ),
   array(
     'title' => '天路导向（粤语）',
-    'url' => 'http://stream.liangyou.net/program/1njcj2-3b2p24',
+    'url' => 'http://www.txly1.net/program/1njcj2-3b2p24',
   ),
   array(
     'title' => '恩典与真理（回民）',
-    'url' => 'http://stream.liangyou.net/program/1njcj0-3b2p20',
+    'url' => 'http://www.txly1.net/program/1njcj0-3b2p20',
   ),
   array(
     'title' => '爱在人间（云南话）',
-    'url' => 'http://stream.liangyou.net/program/1njcj1-3b2p22',
+    'url' => 'http://www.txly1.net/program/1njcj1-3b2p22',
   ),
 );
 require 'vendor/autoload.php';
@@ -229,7 +231,8 @@ foreach ($radios as $key => $radio) {
   // $url = "http://blog.yongbuzhixi.com/test.html";
 
   $html = SimpleHtmlDom\file_get_html($url);
-  if(!$html) continue;
+  if(!$html) { echo '<br/>'.$title;continue;}
+
   $url = $html->find('#div_playlist li', 0);
 
   $title_get = $html->find('#div_playlist li .mp3_title', 0)->plaintext;
@@ -245,6 +248,7 @@ foreach ($radios as $key => $radio) {
 }
 // var_dump($data);
 $file = json_encode($data);
-
-file_put_contents( $file_key , $file );
-echo 'Sucess! Write File :'. $file_key;
+if(!$file){
+  file_put_contents( $file_key , $file );
+  echo 'Sucess! Write File :'. $file_key;
+}
