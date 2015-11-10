21f87fb43be2f7e2a551fa15d335c7add167e904
dale <dale.guo@loweprofero.com>
2015-11-10 16:00:50 +0800

    add ga

--- focus.php
+++ focus.php
@@ -2,3 +2,13 @@
 <h5>►关注：微信回复对于编号即可↓↓↓</h5>
 <a href="http://t.cn/RA2GQFq"><img class="img-responsive" src="fwh.jpg"></a>
 <p>Powered by： <a style="color:#000" href="http://www.yongbuzhixi.com">永不止息</a></p>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-36975988-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
