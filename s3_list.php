9aff65747a6ec3f2f60676f85e7b27c90139741a
dale <dale.guo@loweprofero.com>
2015-12-02 14:49:00 +0800

    cloud up

--- s3_list.php
+++ s3_list.php
@@ -1,25 +1,25 @@
-<pre><?php
+<?php
 //////////////////////////////////////////////////////////////////
 require 'vendor/autoload.php';
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
 $s3 = S3Client::factory(array(
-                'key'    => 'AKIAJZSII3KHTLKFGB5A',
-                'secret' => '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',
+                'key'    => 'AKIAICBSUROM4QGVKMDQ',
+                'secret' => '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',
             ));
-$bucket = 'ybzx';
+$bucket = 'lyaudio';
 
-$response = $s3->if_object_exists($bucket, array('key'=>'test1.txt'));
+// $response = $s3->if_object_exists($bucket, array('key'=>'test1.txt'));
  
 // Success? (Boolean, not a CFResponse object)
-var_dump($response);
-// // Use the high-level iterators (returns ALL of your objects).
-// $objects = $s3->getIterator('ListObjects', array('Bucket' => $bucket));
+// var_dump($response);
+// Use the high-level iterators (returns ALL of your objects).
+$objects = $s3->getIterator('ListObjects', array('Bucket' => $bucket));
 
-// echo "Keys retrieved!\n";
-// foreach ($objects as $object) {
-//     echo 'https://s3.amazonaws.com/ybzx/'.$object['Key'] . "\n";
-// }
+echo "Keys retrieved!<br>";
+foreach ($objects as $object) {
+    echo 'https://s3-ap-southeast-1.amazonaws.com/lyaudio/'.$object['Key'] . "<br>";
+}
 
 // // Use the plain API (returns ONLY up to 1000 of your objects).
 // $result = $s3->listObjects(array('Bucket' => $bucket));
