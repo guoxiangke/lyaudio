26f497f62e05ef030810c62c3a33aab36f4b030e
dale <dale.guo@loweprofero.com>
2015-12-21 16:28:29 +0800

    lyaudio

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 define('S3KEY', 'AKIAICBSUROM4QGVKMDQ');
 define('S3SECRET', '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');
 
-define('UPBUCKETNAME', 'lyaudio');
-define('UPOPNAME', 'lyaudio');
-define('UPOPPASS', 'lyaudiolyaudio');
+define('UPBUCKETNAME', 'lyradio');
+define('UPOPNAME', 'lyradio');
+define('UPOPPASS', 'lyradiolyradio');
 define('CDNLINK', UPBUCKETNAME.'.b0.upaiyun.com');
 
 define('DEBUG', TRUE);
