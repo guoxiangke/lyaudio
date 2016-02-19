7f500731d7d61503c67e9f5ce31d1cc8719128b0
dale <dale.guo@loweprofero.com>
2016-02-19 22:19:14 +0800

    ok

--- focus.php
+++ focus.php
@@ -3,22 +3,12 @@
 <a href="http://dwz.cn/2soCZB"><img class="img-responsive" src="/qrcode.jpg"></a>
 <p>Powered by： <a href="http://www.yongbuzhixi.com">永不止息</a></p>
 <div class="bg-no">
-	<h5><a href="/index.php" style="color:#FFF">线路一</a></h5>
-	<h5><a href="/nizz_play.php" style="color:#FFF">线路二</a></h5>
-	<h5><a href="/download_play.php" style="color:#FFF">线路三</a></h5>
+	<h5><a href="/fm/lyfm.php?fm=603" style="color:#FFF">空中辅导</a></h5>
+	<h5><a href="/fm/lyfm.php?fm=603" style="color:#FFF">良友直播</a></h5>
 	<h5><a href="/player/marrage_training.php" style="color:#FFF">婚姻辅导</a></h5>
 	<!-- <h5><a href="/player/fdjc.php" style="color:#FFF">辅导基础-空中辅导</a></h5> -->
 	<!-- <h5><a href="/player/yong.php" style="color:#FFF">青少年事工</a></h5> -->
 	<h5><a href="/xwz.php" style="color:#FFF">小王子</a></h5>
-	<h5><a href="/fm/index.php?fm=607" style="color:#FFF">欢乐卡恰碰</a></h5>
-	<h5><a href="/fm/index.php?fm=617" style="color:#FFF">現代人的希望</a></h5>
-	<h5><a href="/fm/index.php?fm=620" style="color:#FFF">灵命日粮</a></h5>
-	<h5><a href="/fm/index.php?fm=608" style="color:#FFF">绝妙当家</a></h5>
-	<h5><a href="/fm/index.php?fm=612" style="color:#FFF">书香园地</a></h5>
-	<h5><a href="/fm/index.php?fm=622" style="color:#FFF">圣言盛宴</a></h5>
-	<h5><a href="/fm/index.php?fm=623" style="color:#FFF">齐来颂扬</a></h5>
-	<h5><a href="/fm/index.php?fm=628" style="color:#FFF">空中崇拜</a></h5>
-	<h5><a href="/fm/mrqjs.php" style="color:#FFF">每日亲近神</a></h5>
 </div>
 <style type="text/css">
 	.bg-no h5{
