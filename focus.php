6c0ff4e96a6b5ca252181199c0a8dabcf31e23f7
dale <dale.guo@loweprofero.com>
2016-02-20 09:44:34 +0800

    up

--- focus.php
+++ focus.php
@@ -1,10 +1,12 @@
 <h4>后台收听</h4>
 <h5>►关注：微信回复对于编号即可↓↓↓</h5>
-<a href="http://dwz.cn/2soCZB"><img class="img-responsive" src="/qrcode.jpg"></a>
+<a href="http://mp.weixin.qq.com/s?__biz=MjM5ODQ4NjU4MA==&mid=402309376&idx=1&sn=8e7f77cb6014948fc572f12085521edd#rd"><img class="img-responsive" src="/qrcode.jpg"></a>
 <p>Powered by： <a href="http://www.yongbuzhixi.com">永不止息</a></p>
 <div class="bg-no">
+  <h5><a href="http://www.yongbuzhixi.com" style="color:#FFF">非诚勿点</a></h5>
+  <h5><a href="http://wx.yongbuzhixi.com" style="color:#FFF">主内公众云</a></h5>
 	<h5><a href="/fm/lyfm.php?fm=603" style="color:#FFF">空中辅导</a></h5>
-	<h5><a href="/fm/lyfm.php?fm=603" style="color:#FFF">良友直播</a></h5>
+	<h5><a href="/fm/lyfm.php?fm=602" style="color:#FFF">良友直播</a></h5>
 	<h5><a href="/player/marrage_training.php" style="color:#FFF">婚姻辅导</a></h5>
 	<!-- <h5><a href="/player/fdjc.php" style="color:#FFF">辅导基础-空中辅导</a></h5> -->
 	<!-- <h5><a href="/player/yong.php" style="color:#FFF">青少年事工</a></h5> -->
@@ -21,7 +23,6 @@
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
   ga('create', 'UA-36975988-3', 'auto');
   ga('send', 'pageview');
 
